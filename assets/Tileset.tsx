f36ec356f3f0b6c79781676e76072c309e74d7e5
Raegan <Raegan1414@gmail.com>
2018-10-09 17:09:00 -0400

    Updated tilemap with water texture changes

--- assets/Tileset.tsx
+++ assets/Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Tileset" tilewidth="64" tileheight="64" tilecount="529" columns="23">
- <image source="../../Tileset.png" width="1500" height="1500"/>
+ <image source="Tileset.png" width="1500" height="1500"/>
 </tileset>
