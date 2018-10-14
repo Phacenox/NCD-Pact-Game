d35e18593ddda80c5442088a9c8ed3354e73e24b
Unknown <ruofu.sun@gmail.com>
2018-10-14 18:05:39 -0400

    Modify position of title and start button

--- js/startState.js
+++ js/startState.js
@@ -9,13 +9,15 @@ startState.prototype.preload = function(){
 startState.prototype.create = function(){
 	_background = game.add.sprite(0, 0, "menuscreen");
 	
-	_startButton = game.add.sprite(game.world.centerX, 400, "startbutton");
+	_startButton = game.add.sprite(game.world.centerX, 550, "startbutton");
+    _startButton.anchor.setTo(0.5);
 	_startButton.inputEnabled = true;
 	_startButton.events.onInputDown.add(actiononClick, this);
 	
 	
     
-  this.Title = game.add.text(game.world.centerX-128, 100 , 'Title', { fontSize: '128px', fill: '#460'})
+  this.Title = game.add.text(game.world.centerX, 200 , 'Title', { fontSize: '128px', fill: '#460'})
+  this.Title.anchor.setTo(0.5);
 	
 };
 
