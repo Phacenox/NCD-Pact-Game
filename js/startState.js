9fb43b8ddca68f7e34a0b0efd11aaac3401fda88
Unknown <ruofu.sun@gmail.com>
2018-10-14 18:45:31 -0400

    Remove original title and change the position of startbutton

--- js/startState.js
+++ js/startState.js
@@ -9,15 +9,13 @@ startState.prototype.preload = function(){
 startState.prototype.create = function(){
 	_background = game.add.sprite(0, 0, "menuscreen");
 	
-	_startButton = game.add.sprite(game.world.centerX, 550, "startbutton");
+	_startButton = game.add.sprite(game.world.centerX, 600, "startbutton");
     _startButton.anchor.setTo(0.5);
 	_startButton.inputEnabled = true;
 	_startButton.events.onInputDown.add(actiononClick, this);
 	
 	
-    
-  this.Title = game.add.text(game.world.centerX, 200 , 'Title', { fontSize: '128px', fill: '#460'})
-  this.Title.anchor.setTo(0.5);
+
 	
 };
 
